e144b86b498d45b86931924888ebfe0c1f06282b
Xin ZHANG <xin.zhang@thoughtworks.com>
2023-02-08 14:58:49 +0800

    feat: can delete when hover on task item

--- src/pages/components/TodoItem/index.tsx
+++ src/pages/components/TodoItem/index.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import {Task} from "../../../type";
 import {Checkbox, Divider} from "antd";
 import {useSetRecoilState} from "recoil";
 import {tasksState} from "../../../atoms/tasksState";
+import {CloseOutlined} from "@ant-design/icons";
 
 export interface TodoItemProps {
     task: Task;
@@ -10,6 +11,20 @@ export interface TodoItemProps {
 
 export const TodoItem: React.FC<TodoItemProps> = ({task}) => {
     const setTaskState = useSetRecoilState(tasksState);
+    const [showDeleteBtn, setShowDeleteBtn] = useState(false)
+
+    const deleteBtn = (
+        <CloseOutlined className={'float-right align-middle text-gray-400 mt-1'} id={task.id}
+                       onClick={() => onDelete(task)}/>
+    )
+
+    const onDelete = (task: Task) => {
+        setTaskState(tasks => {
+            return tasks.filter(item => item.id !== task.id)
+        });
+    }
+
+
     const onChange = (checkedValue: any) => {
         setTaskState(tasks => {
             return tasks.map(item => {
@@ -24,9 +39,16 @@ export const TodoItem: React.FC<TodoItemProps> = ({task}) => {
         })
     }
 
-    return (<div>
+    return (<div onMouseEnter={() => {
+            setShowDeleteBtn(true);
+        }}
+                 onMouseLeave={() => {
+                     setShowDeleteBtn(false);
+                 }}>
             <Checkbox className={'checkbox'} onChange={onChange} id={task.id} checked={task.completed}/>
-            <label htmlFor={task.id} className={task.completed ? 'completedTask' : ''}>{task.content}</label>
+            <label htmlFor={task.id}
+                   className={task.completed ? "line-through text-gray-400" : ''}>{task.content}</label>
+            {showDeleteBtn && deleteBtn}
             <Divider className={'divider'}></Divider>
         </div>
     );
